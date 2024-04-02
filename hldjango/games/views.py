90b010f32cda16bf6234391d11b7ccc2e2f0a3c4
dc mouser <mouser@donationcoder.com>
2024-04-02 02:42:14 -0500

    more early work

--- hldjango/games/views.py
+++ hldjango/games/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def homePageView(request):
-    return HttpResponse("Hello, world - from games.")
+    #return HttpResponse("Hello, world - from games.")
+    name = "myworld"
+    return render(request, "base.html", {"name": name})
